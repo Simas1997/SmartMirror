638904ff7501bd2666cc8fdf08676db5e908d7b7
leoluis97 <48104891+leoluis97@users.noreply.github.com>
2019-05-29 15:34:01 -0300

    Add files via upload

--- SmartMirrorFolder/SensorTemperatura.py
+++ SmartMirrorFolder/SensorTemperatura.py
@@ -14,8 +14,8 @@ while(True):
         umidade, temperatura = Adafruit_DHT.read_retry(sensor, pin)
  
         #formata string de umidade e temperatura
-        UmidadeStr = "{0:0.1f}".format(umidade) 
-        TemperaturaStr = "{0:0.1f}*C".format(temperatura) 
+        UmidadeStr = '{0:0.1f}'.format(umidade) 
+        TemperaturaStr = '{0:0.1f}*C'.format(temperatura) 
         print "Leitura "+str(Leitura)+": "+TemperaturaStr+" - "+UmidadeStr
         Leitura = Leitura + 1
     except KeyboardInterrupt:
