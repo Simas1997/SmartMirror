4c64a268abfd28b108ff1e0c55a938d00e448382
Gmmarques97 <49567766+Gmmarques97@users.noreply.github.com>
2019-05-30 22:59:37 -0300

    Add files via upload

---
+++ SmartMirrorFolder/color.py
@@ -0,0 +1,71 @@
+  if (aux.color == "white"):
+      pic_lookup=pic_lookup_white
+  if (aux.color == "red"):
+      pic_lookup=pic_lookup_red
+  if (aux.color == "blue"):
+      pic_lookup=pic_lookup_blue
+  if (aux.color == "yellow"):
+      pic_lookup=pic_lookup_yellow
+
+
+pic_lookup_white = {
+    'clear-day': "assetsColor/Sun.png",  # clear sky day
+    'wind': "assetsColor/Wind.png",   #wind
+    'cloudy': "assetsColor/Cloud.png",  # cloudy day
+    'partly-cloudy-day': "assetsColor/PartlySunny.png",  # partly cloudy day
+    'rain': "assetsColor/Rain.png",  # rain day
+    'snow': "assetsColor/Snow.png",  # snow day
+    'snow-thin': "assetsColor/Snow.png",  # sleet day
+    'fog': "assetsColor/Haze.png",  # fog day
+    'clear-night': "assetsColor/Moon.png",  # clear sky night
+    'partly-cloudy-night': "assetsColor/PartlyMoon.png",  # scattered clouds night
+    'thunderstorm': "assetsColor/Storm.png",  # thunderstorm
+    'tornado': "assests/Tornado.png",    # tornado
+    'hail': "assests/Hail.png"  # hail
+}
+pic_lookup_red = {
+    'clear-day': "assetsColor/SunRed.png",  # clear sky day
+    'wind': "assetsColor/WindRed.png",   #wind
+    'cloudy': "assetsColor/CloudRed.png",  # cloudy day
+    'partly-cloudy-day': "assetsColor/PartlySunnyRed.png",  # partly cloudy day
+    'rain': "assetsColor/RainRed.png",  # rain day
+    'snow': "assetsColor/SnowRed.png",  # snow day
+    'snow-thin': "assetsColor/SnowRed.png",  # sleet day
+    'fog': "assetsColor/HazeRed.png",  # fog day
+    'clear-night': "assetsColor/MoonRed.png",  # clear sky night
+    'partly-cloudy-night': "assetsColor/PartlyMoonRed.png",  # scattered clouds night
+    'thunderstorm': "assetsColor/StormRed.png",  # thunderstorm
+    'tornado': "assests/TornadoRed.png",    # tornado
+    'hail': "assests/HailRed.png"  # hail
+}
+pic_lookup_blue = {
+    'clear-day': "assetsColor/SunBlue.png",  # clear sky day
+    'wind': "assetsColor/WindBlue.png",   #wind
+    'cloudy': "assetsColor/CloudBlue.png",  # cloudy day
+    'partly-cloudy-day': "assetsColor/PartlySunnyBlue.png",  # partly cloudy day
+    'rain': "assetsColor/RainBlue.png",  # rain day
+    'snow': "assetsColor/SnowBlue.png",  # snow day
+    'snow-thin': "assetsColor/SnowBlue.png",  # sleet day
+    'fog': "assetsColor/HazeBlue.png",  # fog day
+    'clear-night': "assetsColor/MoonBlue.png",  # clear sky night
+    'partly-cloudy-night': "assetsColor/PartlyMoonBlue.png",  # scatteBlue clouds night
+    'thunderstorm': "assetsColor/StormBlue.png",  # thunderstorm
+    'tornado': "assests/TornadoBlue.png",    # tornado
+    'hail': "assests/HailBlue.png"  # hail
+}
+pic_lookup_yellow = {
+    'clear-day': "assetsColor/SunYellow.png",  # clear sky day
+    'wind': "assetsColor/WindYellow.png",   #wind
+    'cloudy': "assetsColor/CloudYellow.png",  # cloudy day
+    'partly-cloudy-day': "assetsColor/PartlySunnyYellow.png",  # partly cloudy day
+    'rain': "assetsColor/RainYellow.png",  # rain day
+    'snow': "assetsColor/SnowYellow.png",  # snow day
+    'snow-thin': "assetsColor/SnowYellow.png",  # sleet day
+    'fog': "assetsColor/HazeYellow.png",  # fog day
+    'clear-night': "assetsColor/MoonYellow.png",  # clear sky night
+    'partly-cloudy-night': "assetsColor/PartlyMoonYellow.png",  # scatteYellow clouds night
+    'thunderstorm': "assetsColor/StormYellow.png",  # thunderstorm
+    'tornado': "assests/TornadoYellow.png",    # tornado
+    'hail': "assests/HailYellow.png"  # hail
+}
+
